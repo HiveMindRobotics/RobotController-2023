6fe162c1d9b587a80f0912a5a9bb4e6d526e67d5
snwy <snwy@snwy.me>
2023-12-12 16:42:35 -0500

    new beginnings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.kt
@@ -2,10 +2,26 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+import kotlin.math.sqrt
 
 @TeleOp(name = "Driver Control", group = "Linear Opmode")
 class DriverControl : LinearOpMode() {
+    private fun smooth(x: Float): Double {
+        return if(x > 0) {
+            sqrt(x).toDouble()
+        } else {
+            -sqrt(-x).toDouble()
+        }
+    }
     override fun runOpMode() {
-        // Do stuff
+        val leftMotor = hardwareMap.get(DcMotor::class.java, "leftMotor")
+        val rightMotor = hardwareMap.get(DcMotor::class.java, "rightMotor")
+        waitForStart()
+        while (opModeIsActive()) {
+            // robot is tank drive
+            // left stick controls forward and backward power
+            // left trigger and right
+        }
     }
 }
