46d9678b4a32b21c8f451fac530f8db8c2051b68
NAHTAIV3L <rbeckettvt@gmail.com>
2024-04-04 12:06:06 -0400

    write new mechanum code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.kt
@@ -8,23 +8,33 @@ import com.qualcomm.robotcore.hardware.*
 @Autonomous(name = "AutoRed", group = "Linear Opmode")
 class AutoRed : LinearOpMode() {
     override fun runOpMode() {
-        val leftMotor = hardwareMap.get(DcMotor::class.java, "leftMotor")
-        val rightMotor = hardwareMap.get(DcMotor::class.java, "rightMotor")
-        val armLeft = hardwareMap.get(DcMotor::class.java, "leftBottomArm")
-        val armRight = hardwareMap.get(DcMotor::class.java, "rightBottomArm")
-        val armMiddle = hardwareMap.get(DcMotor::class.java, "middleArm")
-        val armWrist = hardwareMap.get(DcMotor::class.java, "wristArm")
-        val slide = hardwareMap.get(DcMotor::class.java, "slide")
-        val armServo = hardwareMap.get(Servo::class.java, "servo0")
-        val planeServo = hardwareMap.get(Servo::class.java, "servo1")
+        // val leftMotor = hardwareMap.get(DcMotor::class.java, "leftMotor")
+        // val rightMotor = hardwareMap.get(DcMotor::class.java, "rightMotor")
+        // val armLeft = hardwareMap.get(DcMotor::class.java, "leftBottomArm")
+        // val armRight = hardwareMap.get(DcMotor::class.java, "rightBottomArm")
+        // val armMiddle = hardwareMap.get(DcMotor::class.java, "middleArm")
+        // val armWrist = hardwareMap.get(DcMotor::class.java, "wristArm")
+        // val slide = hardwareMap.get(DcMotor::class.java, "slide")
+        // val armServo = hardwareMap.get(Servo::class.java, "servo0")
+        // val planeServo = hardwareMap.get(Servo::class.java, "servo1")
+        
+        val leftFrontMotor = hardwareMap.get(DcMotor::class.java, "leftFrontMotor")
+        val leftBackMotor = hardwareMap.get(DcMotor::class.java, "leftBackMotor")
+        val rightFrontMotor = hardwareMap.get(DcMotor::class.java, "rightFrontMotor")
+        val rightBackMotor = hardwareMap.get(DcMotor::class.java, "rightBackMotor")
 
         waitForStart()
-        leftMotor.power = -10.0
-        rightMotor.power = 10.0
+        
+        leftFrontMotor.power  = 10.0
+        leftBackMotor.power   = -10.0
+        rightFrontMotor.power = 10.0
+        rightBackMotor.power  = 10.0
         
         Thread.sleep(1000)
 
-        leftMotor.power = 0.0
-        rightMotor.power = 0.0
+        leftFrontMotor.power  = 0.0
+        leftBackMotor.power   = 0.0
+        rightFrontMotor.power = 0.0
+        rightBackMotor.power  = 0.0
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControll.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControll.kt
@@ -14,26 +14,38 @@ class DriverControll : LinearOpMode() {
             -sqrt(-x).toDouble()
         }
     }
-
+    
     override fun runOpMode() {
         val leftFrontMotor = hardwareMap.get(DcMotor::class.java, "leftFrontMotor")
         val leftBackMotor = hardwareMap.get(DcMotor::class.java, "leftBackMotor")
         val rightFrontMotor = hardwareMap.get(DcMotor::class.java, "rightFrontMotor")
         val rightBackMotor = hardwareMap.get(DcMotor::class.java, "rightBackMotor")
-        val armLeft = hardwareMap.get(DcMotor::class.java, "leftBottomArm")
-        val armRight = hardwareMap.get(DcMotor::class.java, "rightBottomArm")
-        val armMiddle = hardwareMap.get(DcMotor::class.java, "middleArm")
-        val armWrist = hardwareMap.get(DcMotor::class.java, "wristArm")
-        val slide = hardwareMap.get(DcMotor::class.java, "slide")
-        val armServo = hardwareMap.get(Servo::class.java, "servo0")
-        val planeServo = hardwareMap.get(Servo::class.java, "servo1")
+        // val armLeft = hardwareMap.get(DcMotor::class.java, "leftBottomArm")
+        // val armRight = hardwareMap.get(DcMotor::class.java, "rightBottomArm")
+        // val armMiddle = hardwareMap.get(DcMotor::class.java, "middleArm")
+        // val armWrist = hardwareMap.get(DcMotor::class.java, "wristArm")
+        // val slide = hardwareMap.get(DcMotor::class.java, "slide")
+        // val armServo = hardwareMap.get(Servo::class.java, "servo0")
+        // val planeServo = hardwareMap.get(Servo::class.java, "servo1")
+        
+        waitForStart()
 
-        val drive = gamepad1.left_stick_y.toDouble()
-        val strafe = gamepad1.left_stick_x.toDouble()
-        val twist = gamepad1.right_stick_x.toDouble()
-        leftFrontMotor.power = drive + strafe + twist
-        rightFrontMotor.power = drive - strafe - twist
-        leftBackMotor.power = drive - strafe + twist
-        rightBackMotor.power = drive + strafe - twist
+        var speed_modifier = 1.0
+        
+        while (opModeIsActive()) {
+            val drive = gamepad1.left_stick_y.toDouble()
+            val strafe = -gamepad1.left_stick_x.toDouble()
+            val twist = -gamepad1.right_stick_x.toDouble()
+
+            if (gamepad1.b && !gamepad1.x) 
+                speed_modifier = .5
+            if (gamepad1.x && !gamepad1.b)
+                speed_modifier = 2
+
+            leftFrontMotor.power  = (drive + strafe + twist) * speed_modifier
+            rightFrontMotor.power = (drive - strafe - twist) * speed_modifier
+            leftBackMotor.power   = (drive - strafe + twist) * speed_modifier * -1
+            rightBackMotor.power  = (drive + strafe - twist) * speed_modifier
+        }
     }
 }
