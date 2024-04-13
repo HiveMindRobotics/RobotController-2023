a6900aa8c2e4c1e50e10e3a2968f23e9bd6999f6
NAHTAIV3L <rbeckettvt@gmail.com>
2024-04-13 19:47:59 -0400

    rename variables and add code for linear slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControll.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControll.kt
@@ -16,14 +16,14 @@ class DriverControll : LinearOpMode() {
     }
     
     override fun runOpMode() {
-        val leftFrontMotor = hardwareMap.get(DcMotor::class.java, "leftFrontMotor")
-        val leftBackMotor = hardwareMap.get(DcMotor::class.java, "leftBackMotor")
-        val rightFrontMotor = hardwareMap.get(DcMotor::class.java, "rightFrontMotor")
-        val rightBackMotor = hardwareMap.get(DcMotor::class.java, "rightBackMotor")
-        // val armLeft = hardwareMap.get(DcMotor::class.java, "leftBottomArm")
-        // val armRight = hardwareMap.get(DcMotor::class.java, "rightBottomArm")
-        // val armMiddle = hardwareMap.get(DcMotor::class.java, "middleArm")
-        // val armWrist = hardwareMap.get(DcMotor::class.java, "wristArm")
+        val wheelLeftFront = hardwareMap.get(DcMotor::class.java, "wheelLeftFront")
+        val wheelLeftBack = hardwareMap.get(DcMotor::class.java, "wheelLeftBack")
+        val wheelRightFront = hardwareMap.get(DcMotor::class.java, "wheelRightFront")
+        val wheelRightBack = hardwareMap.get(DcMotor::class.java, "wheelRightBack")
+        val hslideLeft = hardwareMap.get(DcMotor::class.java, "hslideLeft")
+        val hslideRight = hardwareMap.get(DcMotor::class.java, "hslideRight")
+        val vslideLeft = hardwareMap.get(DcMotor::class.java, "vslideLeft")
+        val vslideRight = hardwareMap.get(DcMotor::class.java, "vslideRight")
         // val slide = hardwareMap.get(DcMotor::class.java, "slide")
         // val armServo = hardwareMap.get(Servo::class.java, "servo0")
         // val planeServo = hardwareMap.get(Servo::class.java, "servo1")
@@ -31,6 +31,7 @@ class DriverControll : LinearOpMode() {
         waitForStart()
 
         var speed_modifier = 1.0
+        var sprint = 0.5
         
         while (opModeIsActive()) {
             val drive = gamepad1.left_stick_y.toDouble()
@@ -38,14 +39,37 @@ class DriverControll : LinearOpMode() {
             val twist = -gamepad1.right_stick_x.toDouble()
 
             if (gamepad1.b && !gamepad1.x) 
-                speed_modifier = .5
+                speed_modifier *= .5
             if (gamepad1.x && !gamepad1.b)
-                speed_modifier = 2
+                speed_modifier *= 2
 
-            leftFrontMotor.power  = (drive + strafe + twist) * speed_modifier
-            rightFrontMotor.power = (drive - strafe - twist) * speed_modifier
-            leftBackMotor.power   = (drive - strafe + twist) * speed_modifier * -1
-            rightBackMotor.power  = (drive + strafe - twist) * speed_modifier
+            wheelLeftFront.power  = (drive + strafe + twist) * speed_modifier
+            wheelRightFront.power = (drive - strafe - twist) * speed_modifier
+            wheelLeftBack.power   = (drive - strafe + twist) * speed_modifier * -1
+            wheelRightBack.power  = (drive + strafe - twist) * speed_modifier
+
+
+            if(gamepad2.a)
+                sprint = 1.0
+            else
+                sprint = 0.5
+            
+            if (gamepad2.left_trigger > 0.1 || gamepad2.right_trigger > 0.1) {
+                hslideLeft.power  = -smooth(gamepad2.left_trigger - gamepad2.right_trigger) * sprint
+                hslideRight.power = smooth(gamepad2.left_trigger - gamepad2.right_trigger) * sprint
+            }
+            else {
+                hslideLeft.power = 0.0
+                hslideRight.power = 0.0
+            }
+            if (abs(gamepad2.left_stick_y) > 0.1) {
+                vslideLeft.power  = gamepad2.left_stick_y.toDouble() * sprint
+                vslideRight.power = -gamepad2.left_stick_y.toDouble() * sprint
+            }
+            else {
+                vslideLeft.power = 0.0
+                vslideRight.power = 0.0
+            }
         }
     }
 }
